e4ff31d91b5be1398833248111327998d108226b
José Silva <josegdasilvajunior@gmail.com>
2022-09-04 00:14:27 -0300

    v2.1 up deploy

--- src/main/java/com/gomatch/jogae/JogaeApplication.java
+++ src/main/java/com/gomatch/jogae/JogaeApplication.java
@@ -2,10 +2,11 @@ package com.gomatch.jogae;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class JogaeApplication {
+public class JogaeApplication extends SpringBootServletInitializer {
 
     public static void main(final String[] args) {
         SpringApplication.run(JogaeApplication.class, args);
