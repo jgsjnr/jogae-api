416f99c3bba0c6974fb4da2e88b0a02f316ac5e2
José Silva <josegdasilvajunior@gmail.com>
2022-09-04 00:53:05 -0300

    update fix bugs

--- src/main/java/com/gomatch/jogae/JogaeApplication.java
+++ src/main/java/com/gomatch/jogae/JogaeApplication.java
@@ -2,6 +2,7 @@ package com.gomatch.jogae;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
@@ -10,6 +11,10 @@ public class JogaeApplication extends SpringBootServletInitializer {
 
     public static void main(final String[] args) {
         SpringApplication.run(JogaeApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure (SpringApplicationBuilder application) {
+        return application.sources (JogaeApplication.class);
     }
 
 }
